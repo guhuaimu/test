a7955cc7df7e9e137bfdf2ddc352509b26b619e3
guhuaimu <a2873694794@163.com>
2023-09-04 12:07:32 +0800

    first

---
+++ src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,44 @@
+package com.itheima.domain;
+
+public class User {
+    private Integer id;
+    private String name;
+    private int age;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
